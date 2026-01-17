7aec2953f49300369ea19a74f0299fa1ac56eac9
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2026-01-17 02:36:21 +0200

    FIX: build error

--- src/components/dashboard/candidate/profile-form.tsx
+++ src/components/dashboard/candidate/profile-form.tsx
@@ -262,7 +262,7 @@ export function ProfileForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit, (errors) => console.log("Form Validation Errors:", errors))} className="space-y-8">
                 
                 {/* Personal Information Section */}
                 <div className="space-y-4">
